6f53f65acb497785e3adcda7abfdcf988ad0c9d1
wahend4 <116284086+wahend4@users.noreply.github.com>
2023-12-12 08:47:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serves static files from another folder
 app.use('/css', express.static(path.join(__dirname, '..', 'css')));
